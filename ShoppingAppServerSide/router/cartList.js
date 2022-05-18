f749f3042c1a7c645b9b81b792f04b1b8e84d9b1
Engin Gülek <engingulek0@gmail.com>
2022-05-19 01:44:12 +0300

    Piece:Decrease->Increase

--- ShoppingAppServerSide/router/cartList.js
+++ ShoppingAppServerSide/router/cartList.js
@@ -3,10 +3,12 @@ const CartList = require('../models/CartList');
 const CartProductList = require('../models/CartProductList');
 
 const router = express.Router();
-
+var list = []
 router.get("/getCartList",(req,res)=> {
     CartList.find()
     .then(cartList => {
+        list = cartList
+
         res.json({
             cartList : cartList,
             success : 1
@@ -19,9 +21,24 @@ router.get("/getCartList",(req,res)=> {
 
 
 router.post("/postCartList",(req,res)=>{
-    console.log(req.body.cartListUserId)
-    console.log(req.body.cartList)
-   const cartList = new CartList({
+    console.log(req.body.cartList[0].cartProductId)
+    CartList.find().then(cartList=>{
+        console.log(cartList)
+       var cartfilterList = cartList.filter(x=>x.cartListUserId != 'TestUserId')
+       var cartProductFilterList = cartfilterList[0].cartList.filter(x=>x.cartProductId == req.body.cartList[0].cartProductId )
+       console.log(cartProductFilterList)
+       console.log(cartfilterList[0]._id)
+      
+    })
+  
+    
+
+  
+    /*if (req.body.cartListUserId != ""){
+        console.log(req.body.cartList[0].cartProductId)
+    }*/
+   
+   /*const cartList = new CartList({
         cartListUserId: req.body.cartListUserId,
         cartList : req.body.cartList,
 
@@ -30,7 +47,7 @@ router.post("/postCartList",(req,res)=>{
     res.json({
         success :1,
         message:"success"
-    })
+    })*/
 })
 
 module.exports = router
